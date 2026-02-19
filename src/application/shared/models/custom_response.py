15c2938eca391edac28d2860c81b1cda0a9d60be
jcarloshg <carlosj12336@gmail.com>
2026-02-19 16:59:11 -0600

    creaet the CustomResponde

--- src/application/shared/models/custom_response.py
+++ src/application/shared/models/custom_response.py
@@ -1,21 +1,55 @@
-class CustomResponse:
-    def __init__(self, msg: str = "", is_success: bool = True, data: None = None):
-        self.message = msg
-        self.is_success = is_success
-        self.data = data
+from pydantic import BaseModel
+from typing import Any, Optional
+from fastapi.responses import JSONResponse
+
+
+class CustomResponse(BaseModel):
+    message: str = ""
+    is_success: bool = True
+    data: Optional[Any] = None
+    status_code: int = 200
 
     @staticmethod
-    def success(msg: str = "Success", success: bool = True, data: None = None):
+    def success(msg: str = "Success", success: bool = True, data: Optional[Any] = None):
         return CustomResponse(
-            msg=msg,
+            message=msg,
             is_success=success,
             data=data
         )
 
     @staticmethod
+    def created(msg: str = "Created", data: Optional[Any] = None):
+        return CustomResponse(
+            message=msg,
+            is_success=True,
+            data=data,
+            status_code=201
+        )
+
+    @staticmethod
     def error(msg: str = "Error"):
         return CustomResponse(
-            msg=msg,
+            message=msg,
             is_success=False,
-            data=None
+            data=None,
+            status_code=500
+        )
+
+    def get_response(self):
+        response = {
+            "message": self.message,
+            "is_success": self.is_success,
+            "data": self.data
+        }
+        return response
+
+    def get_JSONResponse(self):
+        response = {
+            "message": self.message,
+            "is_success": self.is_success,
+            "data": self.data
+        }
+        return JSONResponse(
+            content=response,
+            status_code=self.status_code
         )
