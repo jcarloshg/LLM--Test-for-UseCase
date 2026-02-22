a19a27cf69d735f108b123ada46f1f700de339a4
jcarloshg <carlosj12336@gmail.com>
2026-02-21 19:15:11 -0600

    implement the new use cases

--- src/presentation/routes/test_use_cases_route.py
+++ src/presentation/routes/test_use_cases_route.py
@@ -1,15 +1,8 @@
 from fastapi import APIRouter, Request
 
 from src.presentation.controllers import creaet_test_controller
-# from src.presentation.controllers import generate_test_controller
 
 test_use_cases = APIRouter(prefix="/test-use-cases")
-
-
-# @test_use_cases.post("/")
-# async def create_test(request: Request):
-#     response = await generate_test_controller(request)
-#     return response.to_json_response()
 
 
 @test_use_cases.post("/")
